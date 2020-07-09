9dd995b9dc4de85b68630e3022924c8f33bf294c
zhangyao <Reckfull1208@163.com>
2020-07-09 09:57:22 +0800

    框架提交

--- SignalRClient/MainWindow.xaml.cs
+++ SignalRClient/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,57 @@ namespace SignalRClient
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        #region 定义
+        public static string ServiceUrl = "http://localhost:12345/signalr";
+        public IHubProxy hubProxy { get; set; }
+        public HubConnection hubConnection { get; set; }
+        private string ClientId { get; set; }
+        #endregion
+
         public MainWindow()
         {
             InitializeComponent();
+            Closed += MainWindow_Closed;
         }
+
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            if (hubConnection != null)
+            {
+                hubConnection.Stop();
+                hubConnection.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// 连接服务器
+        /// </summary>
+        private async void ConnetService()
+        {
+            hubConnection = new HubConnection(ServiceUrl);
+            //hubConnection.Closed += HubConnection_Closed;
+            hubProxy = hubConnection.CreateHubProxy("SignalRTest");
+
+            //hubProxy.On<string, string>("AddMessage", (name, message) => this.Dispatcher.Invoke(() =>
+            //    //log
+            //    ConnectTb.Text = name + "----" + message
+            // ));
+
+            hubProxy.On("nodify", () => { MessageBox.Show("123"); });
+
+            try
+            {
+                await hubConnection.Start();
+              //  ConnectTb.Text = "连接服务成功";
+            }
+            catch (HttpClientException ex)
+            {
+                //log
+           //     ConnectTb.Text = "连接服务失败";
+                return;
+            }
+        }
+
     }
 }
